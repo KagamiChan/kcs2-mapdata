6527e90443d1d8e86a00a827b547fad13b33819e
Kagami <moe.pascal@gmail.com>
2018-09-28 17:56:10 +0800

    feat: polyfill devtool key bindings on macOS

--- src/main/index.ts
+++ src/main/index.ts
@@ -27,6 +27,25 @@ const createMainWindow = () => {
   })
 
   window.webContents.on('devtools-opened', () => {
+    // Workaround for cut/copy/paste/close keybindings not working in devtools window on OSX
+    // FIXME: https://github.com/electron/electron/issues/11998
+    // credits goes to https://github.com/onivim/oni/pull/2390
+    if (process.platform === 'darwin') {
+      window.webContents.devToolsWebContents.executeJavaScript(`
+        window.addEventListener('keydown', function (e) {
+          if (e.keyCode === 65 && e.metaKey) {
+              document.execCommand('Select All');
+          } else if (e.keyCode === 67 && e.metaKey) {
+              document.execCommand('copy');
+          } else if (e.keyCode === 86 && e.metaKey) {
+              document.execCommand('paste');
+          } else if (e.keyCode === 87 && e.metaKey) {
+              window.close();
+          } else if (e.keyCode === 88 && e.metaKey) {
+              document.execCommand('cut');
+          }
+        });`)
+    }
     window.focus()
     setImmediate(() => {
       window.focus()
