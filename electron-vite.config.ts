6ba7d2e8a0b4aff1b6760a58736d2b1de6754800
かがみ <seki@kagami.me>
2025-05-04 22:23:23 +0800

    chore: electron up

---
+++ electron-vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  main: {
+    plugins: [externalizeDepsPlugin()]
+  },
+  renderer: {
+    plugins: [react()]
+  }
+})
